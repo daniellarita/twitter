5caefba28edef43453a008c0ca231a2018a737fe
Danni <drbaxter821@gmail.com>
2016-10-16 13:03:31 -0400

    updated with db functionality

--- app.js
+++ app.js
@@ -6,46 +6,11 @@ nunjucks.configure('views'); // point nunjucks to the proper directory for templ
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 nunjucks.configure('views', { noCache: true })
-const volleyball = require('volleyball')
-app.use(volleyball)
+
 var routes=require('./routes/index.js')
 app.use('/',routes)
-
-//var bodyParser=require('body-parser')
-
-// app.use(function (req, res, next) {
-//     res.send('Welcome to the any page');
-//     console.log(chalk.green("app.use general here"))
-//     //next()
-// })
-
-// app.use('/special',function (req, res, next) {
-//     // do your logging here
-//     res.send('Welcome to the SpEcIaL page');
-//     console.log(chalk.yellow('Status Code: '+res.statusCode))
-//     next()
-// })
-// var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-// //res.render( 'index', {title: 'Hall of Fame', people: people} );
-// app.get('/', function (req, res) {
-//   res.render('index', {title: "this is the title", people: people})
-//   console.log(chalk.yellow('Status Code: '+res.statusCode))
-//
-// });
 
 app.listen(3000, function () {
   console.log('Twitter app listening on port 3000!');
 
-});
-
-var locals = {
-    title: 'An Example',
-    people: [
-        { name: 'Gandalf'},
-        { name: 'Frodo' },
-        { name: 'Hermione'}
-    ]
-};
-nunjucks.render('index.html', locals, function (err, output) {
-    console.log(output);
 });
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
 var chalk = require('chalk');
 router.use(express.static('public'));
 var bodyParser = require('body-parser')
+const client=require('../db/index.js')
 
 // parse application/x-www-form-urlencoded
 router.use(bodyParser.urlencoded({ extended: false }))
@@ -14,32 +15,56 @@ router.use(bodyParser.urlencoded({ extended: false }))
 router.use(bodyParser.json())
 
 router.post('/tweets', function(req, res) {
-  var name = req.body.name;
-  var text = req.body.text;
-  tweetBank.add(name, text);
+  client.query(`INSERT into users (name) VALUES('${req.body.name}') returning id`,function(err,result){
+    var uId = parseInt(result.rows[0].id);
+    client.query(`INSERT into tweets(userid,content) VALUES(${uId},'${req.body.text}') returning id`,function(err, result){
+      console.log(result);
+    });
+  });
   res.redirect('/');
 });
 
-router.get('/', function (req, res, next) {
-  console.log(chalk.yellow('Status Code: '+res.statusCode))
-  var tweets = tweetBank.list();
-  res.render( 'index', { tweets: tweets , showForm: true});
-//  res.render('index', {showForm: true})
+// router.get('/', function (req, res, next) {
+//   console.log(chalk.yellow('Status Code: '+res.statusCode))
+//     const tweets = client.query('select u.name,u.id, t.content from users u join tweets t on u.id=t.userid', function (err, result) {
+//     if (err) return next(err); // pass errors to Express
+//     var tweets = result.rows;
+//     res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true });
+//   });
+// });
+
+router.get('/', function(req, res) {
+  const tweets = client.query('select u.name,u.id, t.content from users u join tweets t on u.id=t.userid', function (err, result) {
+    if (err) return next(err); // pass errors to Express
+    var tweets = result.rows;
+    res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true });
+  });
 });
+
 router.get('/users/:name', function(req, res) {
-  var name = req.params.name;
-  var list = tweetBank.find( {name: name} );
-  res.render( 'index', { tweets: list , showForm: true, username:name} );
+  console.log(req.params.name, req.params.name.toString())
+  const tweets = client.query(`select u.name, t.content from users u join tweets t on u.id=t.userid where u.name='${req.params.name}'`, function (err, result) {
+    if (err) return next(err); // pass errors to Express
+    var tweets = result.rows;
+    res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true });
+  });
 });
+
 router.get('/tweets/:id', function(req, res) {
-  var id = req.params.id;
-  var list = tweetBank.find( {id: id} );
-  res.render( 'index', { tweets: list , showForm: false} );
+  console.log(req.params.id)
+  const tweets = client.query(`SELECT * FROM tweets where id=${parseInt(req.params.id)}`, function (err, result) {
+    console.log(result);
+    if (err) return next(err); // pass errors to Express
+    var tweets = result.rows;
+    res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true });
+  });
 });
 
 
 router.get('/stylesheets/style.css', function (req, res){
 	res.sendFile(path.join(__dirname,'../public/stylesheets/style.css'));
 })
+
+
 
 module.exports = router;
